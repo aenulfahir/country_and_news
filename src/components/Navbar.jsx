64f7bb881371a6f9a6cf3db14d1d34830ff8c225
aenulfahir <aenulfahir03@gmail.com>
2024-10-09 13:39:08 +0800

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 // src/components/Navbar.jsx  
 import React, { useState } from "react";  
 import { Link } from "react-router-dom";  
-import { Menu as MenuIcon, X as CloseIcon } from "lucide-react"; // Import ikon hamburger dan ikon close  
+import { Menu as MenuIcon, X as CloseIcon } from "lucide-react";  
+import CountryLogo from "../assets/Country.svg"; // Import the logo  
 
 export default function Navbar() {  
     const [isOpen, setIsOpen] = useState(false);  
@@ -11,18 +12,21 @@ export default function Navbar() {
     };  
 
     const handleMenuClick = () => {  
-        setIsOpen(false); // Menutup menu saat item diklik  
+        setIsOpen(false);  
     };  
 
     return (  
         <header className="bg-gray-800">  
             <nav className="container mx-auto flex justify-between items-center p-4">  
                 <div className="flex items-center">  
-                    <span className="text-white text-2xl font-bold">Country and News</span>  
+                    <div className="bg-white  p-2 rounded-full"> {/* White background with padding and rounded corners */}  
+                        <img src={CountryLogo} alt="Country Logo" className="h-10" />  
+                    </div>  
+                    <span className="text-white text-2xl font-bold ml-2">Country and News</span>  
                 </div>  
                 <div className="block lg:hidden">  
                     <button onClick={toggleMenu} className="text-white focus:outline-none">  
-                        {isOpen ? <CloseIcon size={24} /> : <MenuIcon size={24} />} {/* Ganti ikon berdasarkan status */}  
+                        {isOpen ? <CloseIcon size={24} /> : <MenuIcon size={24} />}  
                     </button>  
                 </div>  
                 <ul className={`flex-col lg:flex lg:flex-row lg:space-x-6 ${isOpen ? 'flex' : 'hidden'} lg:items-center lg:flex-row absolute lg:static bg-gray-800 lg:bg-transparent w-full lg:w-auto top-16 left-0 z-10`}>  
